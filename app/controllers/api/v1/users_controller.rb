1f42de15c5036b569abe3e98322bdba124fb78c6
Jennifer Oh <ohjennifer.90@gmail.com>
2019-06-18 17:53:23 -0400

    logout feature working

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,12 @@ class Api::V1::UsersController < ApplicationController
   	render json: @user
   end
 
+  def destroy
+    session[:user_id] = nil
+  end
+
+
+  private
   def user_params
   	params.require(:user).permit(:name, :email)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :create]
+      resources :users, only: [:index, :create, :delete]
       resources :photos, only: [:index, :create]
       resources :locations, only: [:index, :create]
     end
