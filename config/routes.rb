e68bc03c0909245436e490104844a4f4c583ed65
Carly Pecora <carly@carlypecora.com>
2019-06-19 09:21:34 -0400

    add delete route and destroy session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      delete "/users/:id", to: "users#destroy"
       resources :users, only: [:index, :create, :delete]
       resources :photos, only: [:index, :create]
       resources :locations, only: [:index, :create]
