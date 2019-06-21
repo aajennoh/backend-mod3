076c78d25a6daaa01fd0e815003e823b95e5e096
Jennifer Oh <ohjennifer.90@gmail.com>
2019-06-21 13:43:59 -0400

    user-to-user and created show for each user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,9 +5,14 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-  	@user = User.find_or_create_by(user_params)
-  	session[:user_id] = @user.id
-  	render json: @user
+    @user = User.find_or_create_by(user_params)
+    session[:user_id] = @user.id
+    render json: @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
   end
 
   def destroy
@@ -19,6 +24,6 @@ class Api::V1::UsersController < ApplicationController
 
   private
   def user_params
-  	params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       delete "/users/:id", to: "users#destroy"
-      resources :users, only: [:index, :create, :delete]
+      resources :users, only: [:index, :create, :show, :delete]
       resources :photos, only: [:index, :create]
       resources :locations, only: [:index, :create, :update]
     end
